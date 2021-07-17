27727ae4e1618b273e6f41970113ea9b5c6bca9e
Peter Munro <peter@petermunro.org>
2021-07-17 22:14:45 +0100

    Implemented HTTP delete

--- demo-app/src/components/cartool/CarTool.js
+++ demo-app/src/components/cartool/CarTool.js
@@ -5,12 +5,18 @@ import CarTable from './CarTable';
 import Footer from '../generic/Footer';
 import ToolHeader from '../generic/ToolHeader';
 
+const CARS_URL = 'http://localhost:3010/cars';
+
 function CarTool() {
   const [cars, setCars] = useState([]);
   const [editCarId, setEditCarId] = useState(-1);
 
   function deleteCar(id) {
-    setCars(cars.filter((car) => car.id !== id));
+    axios
+      .delete(`${CARS_URL}/${id}`)
+      .then(() => console.log('deleted'))
+      .then(() => setCars(cars.filter((car) => car.id !== id)))
+      .catch(console.error);
   }
 
   function addCar(car) {
@@ -28,7 +34,7 @@ function CarTool() {
   useEffect(
     () =>
       axios
-        .get('http://localhost:3010/cars')
+        .get(CARS_URL)
         .then((res) => setCars(res.data))
         .catch((err) => console.log(err)),
     []
